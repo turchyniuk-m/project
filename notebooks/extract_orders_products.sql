176bbed413b914919016c48d28d64c50e1dd5a5e
Mykhailo Turchyniuk <roycemisha@gmail.com>
2026-03-18 21:01:32 +0100

    Delete notebooks/extract_orders_products.sql

--- notebooks/extract_orders_products.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  o.ga_session_id,
-  p.category,
-  p.name AS product_name,
-  p.price,
-  p.short_description
-FROM `DA.order` AS o
-LEFT JOIN `DA.product` AS p
-  ON p.item_id = o.item_id;
